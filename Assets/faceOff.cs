87d6bde99e3df8a3573b6f7f60a1802e8b2949e4
Timwi <github@timwi.de>
2021-11-21 04:46:01 +0100

    Model revamp

--- Assets/faceOff.cs
+++ Assets/faceOff.cs
@@ -1,10 +1,6 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
-using KModkit;
-using System.Text.RegularExpressions;
 using rnd = UnityEngine.Random;
 
 public class faceOff : MonoBehaviour
