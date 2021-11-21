e263af71b96fda4a4247439bb4bbd6e4b92e4ef2
tasthing <lufarionite@gmail.com>
2021-11-21 03:45:00 -0500

    Module 's done

--- Assets/faceOff.cs
+++ Assets/faceOff.cs
@@ -1,21 +1,27 @@
 using KModkit;
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using UnityEngine;
 using rnd = UnityEngine.Random;
 
 public class faceOff : MonoBehaviour
 {
     public new KMAudio audio;
+    private KMAudio.KMAudioRef mainRef;
     public KMBombInfo bomb;
     public KMBombModule module;
 
     public KMSelectable[] rotationButtons;
+    public KMSelectable submitButton;
     public Renderer[] faceSymbols;
     public Transform polyhedron;
     public Texture[] symbolsA;
     public Texture[] symbolsB;
+    public TextMesh colorblindText;
+    public Color[] polyhedronColors;
     public Color[] buttonColors;
 
     private int[] buttonColorIndices = new int[2];
@@ -39,20 +45,49 @@ public class faceOff : MonoBehaviour
         foreach (KMSelectable button in rotationButtons)
         {
             var ix = Array.IndexOf(rotationButtons, button);
-            button.OnInteract += delegate () { buttonsHeld[ix] = true; audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.BigButtonPress, button.transform); StartCoroutine(Rotate(ix)); return false; };
-            button.OnInteractEnded += delegate { buttonsHeld[ix] = false; audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.BigButtonRelease, button.transform); };
+            button.OnInteract += delegate ()
+            {
+                buttonsHeld[ix] = true;
+                if (mainRef != null)
+                {
+                    mainRef.StopSound();
+                    mainRef = null;
+                }
+                mainRef = audio.HandlePlayGameSoundAtTransformWithRef(KMSoundOverride.SoundEffect.BigButtonPress, button.transform);
+                if (!moduleSolved)
+                    StartCoroutine(Rotate(ix));
+                return false;
+            };
+            button.OnInteractEnded += delegate
+            {
+                buttonsHeld[ix] = false;
+                if (mainRef != null)
+                {
+                    mainRef.StopSound();
+                    mainRef = null;
+                }
+                mainRef = audio.HandlePlayGameSoundAtTransformWithRef(KMSoundOverride.SoundEffect.BigButtonRelease, button.transform);
+            };
         }
+        submitButton.OnInteract += delegate () { Submit(); return false; };
     }
 
     private void Start()
     {
         polyhedron.localRotation = rnd.rotation;
+        polyhedron.GetComponent<Renderer>().material.color = polyhedronColors.PickRandom();
+        for (int i = 0; i < 26; i++)
+        {
+            var rot = faceSymbols[i].transform.localEulerAngles;
+            faceSymbols[i].transform.localEulerAngles = new Vector3(rot.x, rot.y, rnd.Range(0f, 360f));
+        }
         for (int i = 0; i < 2; i++)
             buttonColorIndices[i] = rnd.Range(0, 3);
         var buttonRenders = rotationButtons.Select(x => x.GetComponent<Renderer>()).ToArray();
         for (int i = 0; i < 6; i++)
             buttonRenders[i].material.color = i < 3 ? buttonColors[buttonColorIndices[0]] : buttonColors[buttonColorIndices[1]];
         Debug.LogFormat("[Face Off #{0}] The button colors are {1} on the left and {2} on the right.", moduleId, colorNames[buttonColorIndices[0]], colorNames[buttonColorIndices[1]]);
+        colorblindText.text = GetComponent<KMColorblindMode>().ColorblindModeActive ? buttonColorIndices.Select(x => "CYM"[x]).Join("") : "";
 
         var keyword = keywords[buttonColorIndices[0] * 3 + buttonColorIndices[1]];
         Debug.LogFormat("[Face Off #{0}] The keyword is {1}.", moduleId, keyword[0] + keyword.Substring(1).ToLowerInvariant());
@@ -69,23 +104,29 @@ public class faceOff : MonoBehaviour
 
         mismatchedPosition = rnd.Range(0, 26);
         fakeLetter = alphabet.PickRandom();
-        while (Math.Abs(alphabet.IndexOf(fakeLetter) - alphabet.IndexOf(rearrangedAlphabet[mismatchedPosition])) % 2 != 0)
-            fakeLetter = alphabet.PickRandom();
-        Debug.Log(alphabet.IndexOf(fakeLetter) + ", " + alphabet.IndexOf(rearrangedAlphabet[mismatchedPosition])); // TEMP
+        if (Math.Abs(alphabet.IndexOf(fakeLetter) - alphabet.IndexOf(rearrangedAlphabet[mismatchedPosition])) % 2 != 0)
+            mismatchedPosition = (mismatchedPosition + 1) % 26;
         var secondarySymbolSet = new bool[26];
         for (int i = 0; i < 26; i++)
             secondarySymbolSet[i] = rnd.Range(0, 2) == 0;
-        while (secondarySymbolSet[mismatchedPosition] == secondarySymbolSet[Array.IndexOf(rearrangedAlphabet, fakeLetter)])
-            secondarySymbolSet[mismatchedPosition] = rnd.Range(0, 2) == 0;
+        if (secondarySymbolSet[mismatchedPosition] == secondarySymbolSet[Array.IndexOf(rearrangedAlphabet, fakeLetter)])
+            secondarySymbolSet[mismatchedPosition] = !secondarySymbolSet[mismatchedPosition];
+        var shrinkTheseOnese = new[] { "C2", "R1" };
+        var triangularFaces = new[] { 3, 5, 9, 11, 17, 19, 23, 25 };
         for (int i = 0; i < 26; i++)
+        {
             faceSymbols[i].material.mainTexture = i != mismatchedPosition ? (secondarySymbolSet[i] ? symbolsA : symbolsB)[alphabet.IndexOf(rearrangedAlphabet[i])] : (secondarySymbolSet[i] ? symbolsA : symbolsB)[alphabet.IndexOf(fakeLetter)];
+            var str = i != mismatchedPosition ? rearrangedAlphabet[i].ToString() : fakeLetter.ToString();
+            str += secondarySymbolSet[i] ? "2" : "1";
+            if (!triangularFaces.Contains(i) && shrinkTheseOnese.Contains(str))
+                faceSymbols[i].transform.localScale = new Vector3(.5f, .5f, .5f);
+        }
         Debug.LogFormat("[Face Off #{0}] Where a symbol corresponding to {1} should be displayed, one that corresponds to {2} can be found instead.", moduleId, rearrangedAlphabet[mismatchedPosition], fakeLetter);
         var average = (alphabet.IndexOf(fakeLetter) + alphabet.IndexOf(rearrangedAlphabet[mismatchedPosition])) / 2;
         betweenLetters[0] = alphabet[average];
         betweenLetters[1] = alphabet[(average + 13) % 26];
         values = betweenLetters.Select(x => alphabet.IndexOf(x) + 2).ToArray();
         Debug.LogFormat("[Face Off #{0}] {1} can be found between these two letters, so the numbers the time remaining can be divisible by are {2}.", moduleId, betweenLetters.Join(" and "), values.Join(" and "));
-        Debug.Log((int)bomb.GetTime());
     }
 
     private IEnumerator Rotate(int ix)
@@ -95,40 +136,127 @@ public class faceOff : MonoBehaviour
             switch (ix)
             {
                 case 0:
-                    polyhedron.RotateAround(polyhedron.position, Vector3.up, 3);
+                    polyhedron.RotateAround(polyhedron.position, Vector3.back, 3);
                     break;
                 case 1:
                     polyhedron.RotateAround(polyhedron.position, Vector3.right, 3);
                     break;
                 case 2:
-                    polyhedron.RotateAround(polyhedron.position, Vector3.back, 3);
+                    polyhedron.RotateAround(polyhedron.position, Vector3.down, 3);
                     break;
                 case 3:
-                    polyhedron.RotateAround(polyhedron.position, Vector3.down, 3);
+                    polyhedron.RotateAround(polyhedron.position, Vector3.forward, 3);
                     break;
                 case 4:
                     polyhedron.RotateAround(polyhedron.position, Vector3.left, 3);
                     break;
                 case 5:
-                    polyhedron.RotateAround(polyhedron.position, Vector3.forward, 3);
+                    polyhedron.RotateAround(polyhedron.position, Vector3.up, 3);
                     break;
             }
             yield return null;
         }
     }
 
+    private void Submit()
+    {
+        submitButton.AddInteractionPunch(.25f);
+        audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, submitButton.transform);
+        if (moduleSolved)
+            return;
+        var submittedTime = (int)bomb.GetTime();
+        var div1 = submittedTime % values[0] == 0;
+        var div2 = submittedTime % values[1] == 0;
+        Debug.LogFormat("[Face Off #{0}] You pressed the submit button at {1} ({2} seconds remaining). This is{3} divisible by {4}, and is{5} divisible by {6}.", moduleId, bomb.GetFormattedTime(), submittedTime, div1 ? "" : " not", values[0], div2 ? "" : " not", values[1]);
+        if (submittedTime < values.Min())
+        {
+            Debug.LogFormat("[Face Off #{0}] This time is lower than {1}. Striking and solving simultaneously.", moduleId, values.Min());
+            StartCoroutine(StrikeAndThenSolve());
+        }
+        else if (div1 ^ div2)
+        {
+            Debug.LogFormat("[Face Off #{0}] The time remaining was divisibly by exactly one of the values, so this was a valid time. Module solved!", moduleId);
+            moduleSolved = true;
+            module.HandlePass();
+            audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.CorrectChime, transform);
+            polyhedron.Find("led base").Find("led").GetComponent<Renderer>().material.color = Color.green;
+        }
+        else
+        {
+            Debug.LogFormat("[Face Off #{0}] The time remaining was divisible by {1} of the values, so this was an invalid time.", moduleId, div1 ? "both" : "neither");
+            module.HandleStrike();
+        }
+    }
+
+    private IEnumerator StrikeAndThenSolve()
+    {
+        module.HandleStrike();
+        yield return new WaitForSeconds(.1f);
+        module.HandlePass();
+        polyhedron.Find("led base").Find("led").GetComponent<Renderer>().material.color = Color.yellow;
+    }
+
     // Twitch Plays
 #pragma warning disable 414
-    private readonly string TwitchHelpMessage = "!{0} ";
+    private readonly string TwitchHelpMessage = "!{0} <C/U/R/W/D/L> # [Presses the rotation button with that label for # seconds, can be chained e.g. !{0} C 5 D 4] !{0} submit 58 [Presses the red button the next time the seconds digits of the timer are 58]";
 #pragma warning restore 414
 
     private IEnumerator ProcessTwitchCommand(string input)
     {
-        yield return null;
+        input = input.ToUpperInvariant().Trim();
+        var inputArray = input.Split(' ');
+        var validButtons = new[] { "C", "U", "R", "W", "D", "L" };
+        Match m = Regex.Match(input, @"^(?:submit (\d{1,2}))$", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant);
+        if (m.Success)
+        {
+            yield return null;
+            var value = int.Parse(m.Groups[1].Value);
+            if (value >= 0 && value < 60)
+            {
+                while ((int)bomb.GetTime() % 60 != value)
+                {
+                    yield return null;
+                    yield return "trycancel";
+                };
+            }
+            else
+            {
+                yield return "sendtochaterror Invalid time.";
+                yield break;
+            }
+            submitButton.OnInteract();
+        }
+        else if (inputArray.Length % 2 == 0)
+        {
+            var holdCommands = new List<string[]>();
+            var count = inputArray.Length / 2;
+            var times = new int[count];
+            for (int i = 0; i < count; i++)
+                holdCommands.Add(inputArray.Skip(2 * i).Take(2).ToArray());
+            for (int i = 0; i < count; i++)
+                if (!validButtons.Contains(holdCommands[i][0]) || !int.TryParse(holdCommands[i][1], out times[i]))
+                    yield break;
+            yield return null;
+            for (int i = 0; i < count; i++)
+            {
+                rotationButtons[Array.IndexOf(validButtons, holdCommands[i][0])].OnInteract();
+                yield return new WaitForSeconds(times[i]);
+                rotationButtons[Array.IndexOf(validButtons, holdCommands[i][0])].OnInteractEnded();
+                yield return new WaitForSeconds(1f);
+            }
+        }
+        else
+            yield break;
     }
 
     private IEnumerator TwitchHandleForcedSolve()
     {
-        yield return null;
+        while (!(bomb.GetTime() % values[0] == 0 ^ bomb.GetTime() % values[1] == 0))
+        {
+            yield return true;
+            yield return null;
+        }
+        yield return new WaitForSeconds(.1f);
+        submitButton.OnInteract();
     }
 }
